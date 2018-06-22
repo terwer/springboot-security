fb800edee4068018a64677635ab86e10a72a7cd1
Terwer <youweics@sina.com>
2018-06-22 18:34:12 +0800

    Try to use in memory auth

---
+++ README.md
@@ -0,0 +1,5 @@
+# springboot-security
+
+# 参考
+
+[SpringBoot2.x整合Security5](https://blog.csdn.net/SWPU_Lipan/article/details/80586054)
--- src/main/java/com/terwergreen/springbootsecurity/config/SecurityConfig.java
+++ src/main/java/com/terwergreen/springbootsecurity/config/SecurityConfig.java
@@ -1,12 +1,19 @@
 package com.terwergreen.springbootsecurity.config;
 
 import com.terwergreen.springbootsecurity.util.Constants;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import javax.sql.DataSource;
 
 /**
  * @Author Terwer
@@ -18,6 +25,26 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 @EnableWebSecurity(debug = true)
 @EnableGlobalMethodSecurity(securedEnabled = true)
 public class SecurityConfig {
+
+//    @Autowired
+//    DataSource dataSource;
+
+    @Autowired
+    PasswordEncoder passwordEncoder;
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        // return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+        return new BCryptPasswordEncoder();
+    }
+
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+        //内存中缓存权限数据
+        auth.inMemoryAuthentication().withUser("root").password("123456").roles("USER");
+        //String encodePassword = passwordEncoder.encode("123456");
+        //auth.jdbcAuthentication().dataSource(dataSource).passwordEncoder(passwordEncoder);
+    }
 
     @Configuration
     public static class SecurityConfiguration extends WebSecurityConfigurerAdapter {
